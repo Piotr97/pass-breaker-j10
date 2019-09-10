18b1f699e946568e32239621a1ed7cc3484f1d23
Piotr <deskorolka13@o2.pl>
2019-09-10 12:49:33 +0200

    implementent PassBreaker

--- src/main/java/app/PassBreaker.java
+++ src/main/java/app/PassBreaker.java
@@ -1,4 +1,38 @@
 package app;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigInteger;
+import java.sql.Time;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class PassBreaker {
+    public void passBreaker(String password) {
+        String pass = null;
+        String value;
+        BigInteger counter = BigInteger.ZERO;
+        long start = System.nanoTime();
+
+        while (true) {
+            pass = RandomStringUtils.randomAlphabetic(password.length());
+            counter = counter.add(BigInteger.ONE);
+            if (pass.equalsIgnoreCase(password)) {
+                break;
+            }
+            /*System.out.println(pass + " | " + counter);
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+
+        }
+
+        long stop = System.nanoTime();
+        double elapsedTime = (double) (stop - start) / 1000000000;
+        double convert = TimeUnit.MILLISECONDS.convert((stop - start), TimeUnit.NANOSECONDS);
+        System.out.println("Broken!" + counter + " | " + pass + " at time sec: " + elapsedTime);
+    }
 }
