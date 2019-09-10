78d60bde6c75099b4fd6a2fc8b59a0165d41c80a
Piotr <deskorolka13@o2.pl>
2019-09-10 12:50:06 +0200

    Tu kodze w klasie main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
          System.out.println("Hej Paulina");
+         PassBreaker passBreaker = new PassBreaker();
+         passBreaker.passBreaker("Piotr");
     }
 }
